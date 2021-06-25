6f80774a220c2cbb161e015915929540d62f0563
Lakshna S <78756909+Royely08@users.noreply.github.com>
2021-06-24 22:12:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function Chat(){
+    window.location = "chat.html";
+}
+
+function projects(){
+    window.location = "my_projects.html";
+}
+
+function About_me(){
+    window.location = "About_me.html";
+}
